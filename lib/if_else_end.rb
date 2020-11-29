c8ff4285ab44c0facd88323a3cf49738d241059f
Jacob Hampton <jhampton792@gmail.com>
2020-11-29 18:09:18 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,11 +2,11 @@
 
 time = Time.now 
 
-if time.to_i % 2 == 0 
-  puts "Even!"
-else
-  puts "Odd!"
-end
+##if time.to_i % 2 == 0 
+##  puts "Even!"
+##else
+##  puts "Odd!"
+##end
 
 if time.to_i.even?
   puts "Even!"
